6b27e8aa8ae166c32c2558962b7bc80a29f0ebce
Sergio Gumiel <sergio.gumiel@kairosds.com>
2022-03-01 08:12:42 +0100

    First commit

---
+++ src/test/java/com/kairosds/cursospb2/httpclient/HttpClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kairosds.cursospb2.httpclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
